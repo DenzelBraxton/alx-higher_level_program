69270545ea518a2a22751377760bec5658e5494f
Menor25 <ajirimenor@gmail.com>
2021-11-24 10:38:53 -1200

    function that replaces or adds key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    for i in a_dictionary:
+        if i == key:
+            a_dictionary[i] = value
+        else:
+            a_dictionary[key] = value
+        return a_dictionary
