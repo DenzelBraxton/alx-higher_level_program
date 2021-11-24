22ed0d50200d2390c8d15f22f234c24ded16b88b
Menor25 <ajirimenor@gmail.com>
2021-11-23 22:14:13 -1200

    function that replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    list_len = len(my_list)
-    new_list  = []
-    for i in range(list_len):
+    new_list = []
+    for i in range(len(my_list)):
         if my_list[i] == search:
             new_list.append(replace)
         else:
