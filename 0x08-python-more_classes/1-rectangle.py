3781ecf0d59e1f0bea3fd7893156adcc48580892
Menor25 <ajirimenor@gmail.com>
2021-12-12 22:19:51 -1200

    Error from second task corrected

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Module 0-rectangle
-The module defines an empty rectangle class"""
+"""Module 1-rectangle
+Defines a Rectangle class.
+"""
 
 
 class Rectangle:
-    """This class defines by width and height"""
+    """Rectangle class defined by width and height."""
+
     def __init__(self, width=0, height=0):
-        """Initializes a Rectangle instance and accept the following argument.
+        """Initializes a Rectangle instance.
 
         Args:
             width: width of the rectangle
@@ -14,15 +16,15 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
-        """This retrieves the width of a Rectangle instance."""
+        """Retrieves the width of a Rectangle instance."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """This sets the width of a Rectangle instance
+        """Sets the width of a Rectangle instance
 
         Args:
             value: value of the width, must be a positive integer
@@ -35,12 +37,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """This retrieves the height of a Rectangle instance."""
+        """Retrieves the height of a Rectangle instance."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """This sets the height of a Rectangle instance
+        """Sets the height of a Rectangle instance
 
         Args:
             value: value of the height, must be a positive integer
@@ -50,3 +52,4 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
