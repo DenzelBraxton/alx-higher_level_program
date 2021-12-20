23417baa30e28564c17aebe91aed804de5556985
Menor25 <ajirimenor@gmail.com>
2021-12-19 22:55:33 -1200

    Error corrected

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,7 +5,6 @@ Find returns the list of available attributes and methods of an object
 """
 
 
-
 def lookup(obj):
     """
     Function that returns the list of available attributes and methods of an object
