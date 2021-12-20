e91fe64a238454c6ed50a3906e80de8be4576d6d
Menor25 <ajirimenor@gmail.com>
2021-12-19 22:43:11 -1200

    function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module: 0-lookup
+Find returns the list of available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    Function that returns the list of available attributes and methods of an object
+    args: obj is the object to look into
+    """
+
+    return dir(obj)
