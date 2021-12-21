b7103a5993155ec6ed4a3d92a524fc5538ee8b7d
Menor25 <ajirimenor@gmail.com>
2021-12-20 21:32:15 -1200

    Error corrected

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -21,4 +21,4 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         
         if value <= 0:
-            raise ValueError("{} must be an integer".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
