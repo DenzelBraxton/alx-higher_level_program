557f6f5665a3b8663f5348d34b0726f89fd95191
Menor25 <ajirimenor@gmail.com>
2021-12-21 12:09:18 -1200

    a function that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,8 +2,6 @@
 """Module: 0-read_file
 This is a function that reads a text file (UTF8) and prints it to stdout
 """
-
-
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout
 
