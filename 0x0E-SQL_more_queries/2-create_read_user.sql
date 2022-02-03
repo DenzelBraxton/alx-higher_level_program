1991655bafe565b4ebd81a7d9cc985ad0547c08c
Menor25 <ajirimenor@gmail.com>
2022-02-02 22:01:35 -1200

    script that creates the database hbtn_0d_2 and the user user_0d_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2
 -- creating a database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_02;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 --next, creating user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- next, grant SELECT privileges to a user
