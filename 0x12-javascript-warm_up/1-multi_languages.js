0328230b15fd2a15ab0e3f1057a150e62261670b
Menor25 <ajirimenor@gmail.com>
2022-02-28 21:41:21 +0100

    a script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//script that prints 3 lines
+
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+console.log (myVar1);
+console.log (myVar2);
+console.log (myVar3);
