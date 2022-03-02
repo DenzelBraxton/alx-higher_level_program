0e435f68794941669d1136196845c85d3d67173c
Menor25 <ajirimenor@gmail.com>
2022-03-02 14:47:04 +0100

    a script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 // a script that prints a square
 
 if (isNaN(process.argv[2])) {
-    console.log('Missing size');
-  } else {
-    for (let i = 0; i < parseInt(process.argv[2]); i++) {
-      console.log('X'.repeat(parseInt(process.argv[2])));
-    }
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
   }
+}
