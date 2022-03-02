60e8e43ec81c5c70b2a2b886fd483aa5c5909138
Menor25 <ajirimenor@gmail.com>
2022-03-02 11:51:03 +0100

    a script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 // a script that prints the addition of 2 integers
 
 function add (a, b) {
-  return(parseInt(a) + parseInt(b));
+  return parseInt(a) + parseInt(b);
 }
 
 console.log(add(process.argv[2], process.argv[3]));
