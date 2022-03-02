ea6640d23b5b248e50698543a373c021e69bd4b9
Menor25 <ajirimenor@gmail.com>
2022-03-02 14:42:48 +0100

    a script that searches the second biggest integer in the list of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,8 @@
 // a script that searches the second biggest integer in the list of arguments
 
 if (process.argv.length <= 3) {
-    console.log(0);
-  } else {
-    const list = process.argv.sort();
-    console.log(list.reverse()[1]);
-  }
+  console.log(0);
+} else {
+  const list = process.argv.sort();
+  console.log(list.reverse()[1]);
+}
