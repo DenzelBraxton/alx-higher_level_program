db18c807450c34819308ebe7ce13a20b564c746a
Menor25 <ajirimenor@gmail.com>
2022-09-10 17:40:51 +0100

    Error fixed in fetching data

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+"""
+python script that lists all states from the database hbtn_0e_0_usa with
+a given name
+"""
 
-from email import charset
+import MySQLdb
 from sys import argv
 
-
-if __name__ == '__main__':
-
-    import MySQLdb
-    import sys
-
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}'\
-                ORDER BY states.id ASC".format(sys.argv[4]))
-    rows = cur.fetchall()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+    rows = cursor.fetchall()
     for row in rows:
         if row[1] == argv[4]:
             print(row)
-    cur.close()
+    cursor.close()
     db.close()
