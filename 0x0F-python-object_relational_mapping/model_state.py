31fe8e66fe64d23725b618788467136a14062f77
Menor25 <ajirimenor@gmail.com>
2022-09-10 22:41:57 +0100

    Error corrected with model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,6 +11,5 @@ class State(Base):
     """Class State"""
 
     __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True,
-                primary_key=True, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
