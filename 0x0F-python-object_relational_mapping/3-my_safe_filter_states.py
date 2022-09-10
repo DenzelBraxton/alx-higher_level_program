14801da981bceb0295d39562df54965df869fb4d
Menor25 <ajirimenor@gmail.com>
2022-09-10 21:48:19 +0100

    Fixed little error in the query

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,13 +4,12 @@ a script that takes in an argument and displays all values in the states table o
 and is safe from SQL injections
 """
 
-from email import charset
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost', port='3306', user=argv[1], 
-                        passwd=argv[2], db=argv[3], charset='utf8')
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], 
+                        passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
                     (argv[4],))
