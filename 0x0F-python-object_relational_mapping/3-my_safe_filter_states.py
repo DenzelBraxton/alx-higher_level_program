ed9f2ee13a7fa23c6ce100a1f9020fb2cb6f3c30
Menor25 <ajirimenor@gmail.com>
2022-09-10 21:43:57 +0100

    Fixed little error in the query

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument
-and is safe from SQL injections"""
+"""
+a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument
+and is safe from SQL injections
+"""
+
+from email import charset
+import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
-
-    import MySQLdb
-    import sys
-
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s\
-                ORDER BY states.id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
+    db = MySQLdb.connect(host='localhost', port='3306', user=argv[1], 
+                        passwd=argv[2], db=argv[3], charset='utf8')
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                    (argv[4],))
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
+    cursor.close()
+    db.close()
