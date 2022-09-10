9b9ca84dbf406fb4fa7cd07ab38d4dcfbabf36ff
Menor25 <ajirimenor@gmail.com>
2022-09-10 22:52:22 +0100

    Error corrected with model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,12 @@
 """a python file that contains the class definition of a State and an instance Base = declarative_base()"""
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import (create_engine, Column, Integer, String)
+
+import MySQLdb
+from sys import argv
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
 Base = declarative_base()
 
@@ -13,3 +18,7 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
