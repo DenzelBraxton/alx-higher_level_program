8d3511b4a9f263adb701d2acc37b65d4a2faa913
Menor25 <ajirimenor@gmail.com>
2022-09-11 11:11:52 +0100

    adds the State object “Louisiana” to the database

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -18,18 +18,10 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    # Session = sessionmaker(bind=engine)
-    # session = Session()
-
-    # louisiana = State(name="Louisiana")
-    # session.add(louisiana)
-    # session.commit()
-    # print(louisiana.id)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name="Louisiana")
-    session.add(new_state)
-    print(session.query(State).filter_by(name="Louisiana").first().id)
+
+    louisiana = State(name="Louisiana")
+    session.add(louisiana)
     session.commit()
-    session.close()
+    print(louisiana.id)
