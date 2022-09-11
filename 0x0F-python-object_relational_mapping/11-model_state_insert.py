61d65a177240d520da9b53c40a5043c2b3b7ae2a
Menor25 <ajirimenor@gmail.com>
2022-09-11 10:46:53 +0100

    adds the State object “Louisiana” to the database

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -19,8 +19,7 @@ if __name__ == "__main__":
     session = Session()
     new_state = State(name='Louisiana')
     session.add(new_state)
+    session.commit()
     state = session.query(State).filter_by(name='Louisiana').first()
     print(str(state.id))
-    session.commit()
     session.close()
-    
