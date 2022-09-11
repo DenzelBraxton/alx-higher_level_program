1e25710149a9a040077ec5b2159d6cbb16ffab6b
Menor25 <ajirimenor@gmail.com>
2022-09-11 12:27:41 +0100

    file that contain the class definition of a city

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """
-prints all City objects from the database
+prints all City objects from a database
 """
 
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 from model_city import City
 
+
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
-                                                                        argv[2],
-                                                                        argv[3]))
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-
-    row = session.query(City, State).filter(City.state_id == State.id)\
-                                            .order_by(City.id).all()
-    for city, state in row:
-        print("{}: ({}) ({}".format(state.name, city.id, city.name))
+    rows = session.query(City, State).filter(City.state_id == State.id)\
+                                     .order_by(City.id).all()
+    for city, state in rows:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-city class
+contains the class City
 """
 
 import sqlalchemy
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base, State
 
 
@@ -13,4 +13,4 @@ class City(Base):
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("state_id"))
+    state_id = Column(Integer, ForeignKey('states.id'))
