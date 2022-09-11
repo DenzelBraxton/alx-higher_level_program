9150bcd5a87cb4dc5245b1f3e01db84c61791d87
Menor25 <ajirimenor@gmail.com>
2022-09-11 09:51:36 +0100

    Error corrected in task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-a script that prints the State object with the name 
-passed as argument from the database hbtn_0e_6_usa
+prints the State object with the name passed as argument from a database
 """
 
 import sqlalchemy
@@ -11,18 +10,16 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
-                                                                        argv[2],
-                                                                        argv[3]))
-    
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states = session.query(State).filter_by(name=argv[4]).first()
-
-    if states is not None:
-        print(str(states.id))
+    state = session.query(State).filter_by(name=argv[4]).first()
+    if state is not None:
+        print(str(state.id))
     else:
         print("Not found")
     session.close()
+    
