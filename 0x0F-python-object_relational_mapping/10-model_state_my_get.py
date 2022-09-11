ddbf96328878b7fde870488ecf64af91e9a47bcd
Menor25 <ajirimenor@gmail.com>
2022-09-11 09:42:08 +0100

    Error corrected in task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""a script that prints the State object with the name 
-passed as argument from the database hbtn_0e_6_usa"""
+"""
+a script that prints the State object with the name 
+passed as argument from the database hbtn_0e_6_usa
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
 
 if __name__ == "__main__":
-
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                                                                        argv[2],
+                                                                        argv[3]))
+    
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    session = Session(engine)
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
-    if state:
-        print("{}".format(state.id))
+    states = session.query(State).filter(name=argv[4]).first()
+
+    if states is not None:
+        print(str(states.id))
     else:
         print("Not found")
     session.close()
-
+    
