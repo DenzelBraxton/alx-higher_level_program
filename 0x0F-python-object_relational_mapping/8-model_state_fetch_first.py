c7c60aa543865b92cbce11748c417a42e078e036
Menor25 <ajirimenor@gmail.com>
2022-09-11 08:55:47 +0100

    Query modified to pick first item on the table

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""a script that prints the first State object from the database hbtn_0e_6_usa"""
+"""
+list the first State object from a database
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
 
 if __name__ == "__main__":
-
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session, sessionmaker
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2],
-                                   argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-    first = session.query(State).order_by(State.id).first()
-    if first is not None:
-        print("{}: {}".format(first.id, first.name))
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state is not None:
+        print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
     session.close()
