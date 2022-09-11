7089e542f2674e980b31c95dc723cd53da463461
Menor25 <ajirimenor@gmail.com>
2022-09-11 11:26:24 +0100

    a script that changes the name of a State object from the database

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-a script that changes the name of a State object from the database 
+changes the name of the State object where id=2 to New Mexico from a database
 """
 
 import sqlalchemy
@@ -9,15 +9,16 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
-                                                                        argv[2],
-                                                                        argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
     state = session.query(State).filter_by(id=2).first()
     state.name = "New Mexico"
     session.commit()
     session.close()
+    
