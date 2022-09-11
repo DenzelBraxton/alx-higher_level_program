0a23f9ea06a63c9b2be4f07cd6043a8006df60a1
Menor25 <ajirimenor@gmail.com>
2022-09-11 12:00:27 +0100

    a script that deletes all State objects with a name containing the letter a from the database

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,8 +12,7 @@ from model_state import Base, State
 if __name__ == "__main__":
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                                     argv[2],
-                                                                    argv[3]),
-                                                        pool_pre_ping=True)
+                                                                    argv[3]))
     Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     session = Session()
