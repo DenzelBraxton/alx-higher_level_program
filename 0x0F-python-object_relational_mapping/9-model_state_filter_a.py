5d885d3a4d5e2c9ad1cabfa679eeedd7764a494f
Menor25 <ajirimenor@gmail.com>
2022-09-11 09:00:04 +0100

    Code updated to fix error

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa"""
+"""
+a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
