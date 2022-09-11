c3e81dc7a1233394ab574f0f4ce68c772729705d
Menor25 <ajirimenor@gmail.com>
2022-09-11 21:23:56 +0100

    a script that lists all State objects, and corresponding City objects, contained in the database

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-
 """
-a script that lists all State objects, and corresponding City objects, contained in the database
+lists all State objects and corresponding City objects contained in a database
 """
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
+from sys import argv
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
                                                                     argv[2],
                                                                     argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-
-    row = session.query(State).all()
-
-    for state in row:
+    rows = session.query(State).all()
+    for state in rows:
         print("{}: {}".format(state.id, state.name))
-        for city in state.id:
-            print("{}: {}".format(city.id, city.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
     session.close()
-    
