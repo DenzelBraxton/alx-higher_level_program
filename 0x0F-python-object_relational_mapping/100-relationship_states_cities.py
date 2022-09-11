5f231a36332d36555403c717824606155e987189
Menor25 <ajirimenor@gmail.com>
2022-09-11 12:54:29 +0100

    script that lists all State objects, and corresponding City objects, contained in the database

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that creates the State “California” with the City “San Francisco” from the database
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                                                                        argv[2],
+                                                                        argv[3]))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.add(State(name= "California", city=City(name="San Francisco")))
+    session.commit()
+    session.close()
