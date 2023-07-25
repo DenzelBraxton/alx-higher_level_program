a751379bd8d9502166e776e8d526df1b13de60f7
DenzelBraxton <99493584+DenzelBraxton@users.noreply.github.com>
2023-07-25 14:57:39 +0300

    0x14-Java Script Web Scrapping

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const starWarsUrl = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(starWarsUrl, function (_err, _res, body) {
+  const characters = JSON.parse(body).characters;
+
+  for (let i = 0; i < characters.length; ++i) {
+    request(characters[i], function (_cErr, _cRes, cBody) {
+      console.log(JSON.parse(cBody).name);
+    });
+  }
+});
+
